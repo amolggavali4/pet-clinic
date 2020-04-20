22a607fc0121d74e6997c3376e1dad59ebff7ee1
Amol <amol.g.gavali@gmail.com>
2020-04-20 21:55:49 +0530

    Added Owner controller and index page. Closes #13

--- pet-clinic-web/src/main/java/guru/springframework/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/controller/OwnerController.java
@@ -2,11 +2,14 @@ package guru.springframework.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
-    @RequestMapping({"owners","owners/index","/owners/index.html"})
+    @RequestMapping({"","/","/index","/index.html"})
     public String listOwners(){
+        System.out.println("Owner controller");
         return "owners/index";
     }
 }
