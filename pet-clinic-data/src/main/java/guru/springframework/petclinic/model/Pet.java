3c1425f6cd602ec057f638608a544610f1d90776
Amol <amol.g.gavali@gmail.com>
2020-04-26 22:35:35 +0530

    Updated owner with JPA mappings. Closes #36

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Pet.java
@@ -1,11 +1,20 @@
 package guru.springframework.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
-
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity{
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @ManyToOne
+    @JoinColumn(name = "petType_id")
     private PetType type;
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+    @Column(name = "name")
     private String name;
 
     public LocalDate getBirthDate() {
