0ff04e74a0a9a7316a5533878d4362012492df94
Amol <amol.g.gavali@gmail.com>
2020-04-26 22:51:58 +0530

    Updated visits with JPA mappings. Closes #38

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Pet.java
@@ -2,6 +2,9 @@ package guru.springframework.petclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity{
@@ -17,6 +20,8 @@ public class Pet extends BaseEntity{
     @Column(name = "name")
     private String name;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
     public LocalDate getBirthDate() {
         return birthDate;
     }
@@ -47,5 +52,13 @@ public class Pet extends BaseEntity{
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
