8bd8c65d600f660a44039bc1ecbb1c1da8e4bac2
Amol <amol.g.gavali@gmail.com>
2020-04-27 01:10:58 +0530

    Created spring data JPA Pet service Closes #43

---
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/jpa/PetJPAService.java
@@ -0,0 +1,46 @@
+package guru.springframework.petclinic.services.jpa;
+
+import guru.springframework.petclinic.model.Pet;
+import guru.springframework.petclinic.repositories.PetRepository;
+import guru.springframework.petclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+@Service
+@Profile("jpa")
+public class PetJPAService implements PetService {
+    private final PetRepository petRepository;
+
+    public PetJPAService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().iterator().forEachRemaining(pets::add);
+        return pets;
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
