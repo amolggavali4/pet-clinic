bb7924ef6dc3b70406bac7df941410a272a7a18e
Amol <amol.g.gavali@gmail.com>
2020-04-30 00:56:58 +0530

    Added project Lombok support. Closes #51

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/PetType.java
@@ -1,19 +1,20 @@
 package guru.springframework.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "pet_types")
 public class PetType extends BaseEntity{
     @Column(name = "name")
     private String name;
 
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Speciality.java
@@ -1,20 +1,20 @@
 package guru.springframework.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "specialty")
 public class Speciality extends BaseEntity{
     @Column(name = "description")
     private String description;
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
